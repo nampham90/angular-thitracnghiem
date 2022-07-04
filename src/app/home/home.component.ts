cc8fc26e54380e5edee34d2c9aba13f555c41295
nguyenvietsong <satthu147258369@gmail.com>
2022-07-04 21:23:13 +0700

    test commit home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { UserService } from '../_services/user.service';
 })
 export class HomeComponent implements OnInit {
 
-  content: string;
+  content: string; // content home
 
   constructor(private userService: UserService) { }
 
